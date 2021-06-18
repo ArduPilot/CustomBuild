83456ad752ae6bfe6f483c874472b13c8bd72e92
willpiper <willpiper24@gmail.com>
2021-06-18 18:38:46 +0100

    updated file names

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ from flask import Flask # using flask as the framework
 from flask import render_template
 from flask import request
 
-from terrain_gen import add_offset
-
 # Directory of this file
 this_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -23,21 +21,6 @@ tile_path = os.path.join(this_path, '..', 'data', 'tiles')
 
 # The output folder for all gzipped terrain requests
 app = Flask(__name__, static_url_path='/terrain', static_folder=output_path,)
-
-def clamp(n, smallest, largest):
-    return max(smallest, min(n, largest))
-
-def getDatFile(lat, lon):
-    '''Get file'''
-    if lat < 0:
-        NS = 'S'
-    else:
-        NS = 'N'
-    if lon < 0:
-        EW = 'W'
-    else:
-        EW = 'E'
-    return "%c%02u%c%03u.DAT.gz" % (NS, min(abs(int(lat)), 99), EW, min(abs(int(lon)), 999))
 
 def compressFiles(fileList, uuidkey):
     # create a zip file comprised of dat.gz tiles
@@ -104,30 +87,6 @@ def generate():
 
         # UUID for this terrain generation
         uuidkey = str(uuid.uuid1())
-
-        # get a list of files required to cover area
-        filelist = []
-        done = set()
-        for dx in range(-radius, radius):
-            for dy in range(-radius, radius):
-                (lat2, lon2) = add_offset(lat*1e7, lon*1e7, dx*1000.0, dy*1000.0)
-                lat_int = int(round(lat2 * 1.0e-7))
-                lon_int = int(round(lon2 * 1.0e-7))
-                tag = (lat_int, lon_int)
-                if tag in done:
-                    continue
-                done.add(tag)
-                # make sure tile is inside the 60deg latitude limit
-                if abs(lat_int) <= 60:
-                    filelist.append(os.path.join(tile_path, getDatFile(lat_int, lon_int)))
-                else:
-                    outsideLat = True
-
-        # make sure tile is inside the 60deg latitude limit
-        if abs(lat_int) <= 60:
-            filelist.append(os.path.join(tile_path, getDatFile(lat_int, lon_int)))
-        else:
-            outsideLat = True
 
         # remove duplicates
         filelist = list(dict.fromkeys(filelist))
