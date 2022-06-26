d248cea870e3be02d73c2323125d81330111f3ed
Michelle Rossouw <michelleros128@gmail.com>
2022-06-26 14:04:29 +1000

    Add Blimp and Heli as vehicle options

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ os.nice(20)
 
 appdir = os.path.dirname(__file__)
 
-VEHICLES = [ 'Copter', 'Plane', 'Rover', 'Sub', 'Tracker' ]
+VEHICLES = [ 'Copter', 'Plane', 'Rover', 'Sub', 'Tracker', 'Blimp', 'Heli' ]
 default_vehicle = 'Copter'
 
 def get_boards_from_ardupilot_tree():
