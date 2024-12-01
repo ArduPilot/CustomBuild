e8356c1aecc5b4ac60f0f634ba9bb054be34f72d
Shiv Tyagi <shivtyagi3015@gmail.com>
2024-12-01 17:11:03 +0530

    utils: add RateLimiter module

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,8 @@
 from .taskrunner import TaskRunner
+from .ratelimiter import RateLimiter, RateLimitExceededException
 
 __all__ = [
     "TaskRunner",
+    "RateLimiter",
+    "RateLimitExceededException"
 ]
