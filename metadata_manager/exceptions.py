c3e05ac0b859c8cb6b9686515ec652ea3d069c18
Shiv Tyagi <shivtyagi3015@gmail.com>
2025-04-20 16:46:17 +0530

    metadata_manager: get rid of exceptions.py
    They are nowhere being used now. The need for TooManyInstancesError can be fulfilled using the RuntimeError exception.

--- metadata_manager/exceptions.py
+++
@@ -1,8 +0,0 @@
-class MetadataManagerException(Exception):
-    pass
-
-
-class TooManyInstancesError(MetadataManagerException):
-    def __init__(self, name: str):
-        message = f"{name} should be a singleton."
-        super().__init__(message)
