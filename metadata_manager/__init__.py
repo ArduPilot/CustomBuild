307d4feddaa3ea204c69abaddf866e17473069f0
Shiv Tyagi <shivtyagi3015@gmail.com>
2025-05-05 10:23:04 +0530

    metadata_manager: add VehiclesManager

--- metadata_manager/__init__.py
+++ metadata_manager/__init__.py
@@ -7,8 +7,66 @@ from .ap_src_meta_fetcher import (
     APSourceMetadataFetcher,
 )
 
+from .vehicles_manager import (
+    VehiclesManager,
+    Vehicle,
+)
+
 __all__ = [
     "APSourceMetadataFetcher",
     "VersionsFetcher",
     "RemoteInfo",
+    "VehiclesManager",
+    "Vehicle",
 ]
+
+vehicles_manager = VehiclesManager()
+
+vehicles_manager.add_vehicle(
+    Vehicle(
+        name="Copter",
+        waf_build_command="copter"
+    )
+)
+
+vehicles_manager.add_vehicle(
+    Vehicle(
+        name="Plane",
+        waf_build_command="plane"
+    )
+)
+
+vehicles_manager.add_vehicle(
+    Vehicle(
+        name="Rover",
+        waf_build_command="rover"
+    )
+)
+
+vehicles_manager.add_vehicle(
+    Vehicle(
+        name="Sub",
+        waf_build_command="sub"
+    )
+)
+
+vehicles_manager.add_vehicle(
+    Vehicle(
+        name="Heli",
+        waf_build_command="heli"
+    )
+)
+
+vehicles_manager.add_vehicle(
+    Vehicle(
+        name="Blimp",
+        waf_build_command="blimp"
+    )
+)
+
+vehicles_manager.add_vehicle(
+    Vehicle(
+        name="Tracker",
+        waf_build_command="AntennaTracker"
+    )
+)
