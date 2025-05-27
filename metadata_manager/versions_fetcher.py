7a880688e3ba0d072216e20d8d81a8baf09a6753
Shiv Tyagi <shivtyagi3015@gmail.com>
2025-05-27 18:20:29 +0530

    metadata_manager: make RemoteInfo serializable

--- metadata_manager/versions_fetcher.py
+++ metadata_manager/versions_fetcher.py
@@ -36,6 +36,12 @@ class RemoteInfo:
         self.name = name
         self.url = url
 
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'url': self.url,
+        }
+
 
 class VersionsFetcher:
     """
